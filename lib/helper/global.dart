311f3a130cbf9c3d3423d2e2a922ee33caa17ed9
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-06 17:51:40 -0300

    Update global.dart

--- lib/helper/global.dart
+++ lib/helper/global.dart
@@ -4,6 +4,6 @@ const appName = 'Ai Assistant';
 
 late Size mq;
 
-String apiKey = 'NAO_INJETADO';
-String claudeKey = 'NAO_INJETADO';
-String groqKey = 'NAO_INJETADO';
+String apiKey = '';
+String claudeKey = '';
+String groqKey = '';
