21ff7115c9ddf76092c76121bc30c1f4077e0b6d
nadiaperesoficial-hash <nadiaperesoficial@gmail.com>
2026-06-06 11:56:35 -0300

    Update message.dart

--- lib/model/message.dart
+++ lib/model/message.dart
@@ -1,8 +1,13 @@
 class Message {
   String msg;
   final MessageType msgType;
+  final String? aiProvider;
 
-  Message({required this.msg, required this.msgType});
+  Message({
+    required this.msg,
+    required this.msgType,
+    this.aiProvider,
+  });
 }
 
 enum MessageType { user, bot }
