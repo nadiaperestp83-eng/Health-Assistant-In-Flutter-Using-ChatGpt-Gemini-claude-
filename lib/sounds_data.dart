9d5da87ed65c03518c4251e9635485dec910ad02
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-06-08 07:34:20 -0300

    Create sounds_data.dart

---
+++ lib/sounds_data.dart
@@ -0,0 +1,9 @@
+// lib/sounds_data.dart
+
+final List<String> trilhasRefugio = [
+  "https://freesound.org/data/previews/554/554674_10672016-lq.mp3",
+  "https://freesound.org/data/previews/829/829030_15494489-lq.mp3",
+  "https://freesound.org/data/previews/834/834397_13337926-lq.mp3",
+  "https://freesound.org/data/previews/572/572245_11701550-lq.mp3",
+  "https://freesound.org/data/previews/423/423946_7749872-lq.mp3",
+];
