de1c945a6e46d03839cc856bbe6cf0aa21f26257
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-06-09 07:14:36 -0300

    Update chatbot_feature.dart

--- lib/screen/feature/chatbot_feature.dart
+++ lib/screen/feature/chatbot_feature.dart
@@ -11,8 +11,6 @@ import '../../elevenlabs_service.dart';
 import '../../helper/global.dart';
 import '../../helper/my_dialog.dart';
 import '../../widget/message_card.dart';
-import 'image_feature.dart';
-import 'translator_feature.dart';
 
 class ChatBotFeature extends StatefulWidget {
   const ChatBotFeature({super.key});
@@ -27,17 +25,9 @@ class _ChatBotFeatureState extends State<ChatBotFeature> {
   final _stt = SpeechToText();
   final _player = AudioPlayer();
 
-  int _selectedTab = 0;
   bool _isListening = false;
   bool _ttsEnabled = true;
   bool _isSpeaking = false;
-
-  final _tabs = [
-    {'icon': Icons.chat_bubble_rounded, 'label': 'Chat'},
-    {'icon': Icons.image_rounded, 'label': 'Imagem'},
-    {'icon': Icons.translate_rounded, 'label': 'Tradutor'},
-    {'icon': Icons.videocam_rounded, 'label': 'Vídeo'},
-  ];
 
   @override
   void initState() {
@@ -63,7 +53,6 @@ class _ChatBotFeatureState extends State<ChatBotFeature> {
     if (cleanText.isEmpty) return;
 
     await _tts.stop();
-
     setState(() => _isSpeaking = true);
 
     final Uint8List? audioBytes = await ElevenLabsService.sintetizar(cleanText);
@@ -154,15 +143,9 @@ class _ChatBotFeatureState extends State<ChatBotFeature> {
           icon: const Icon(Icons.menu, color: Colors.black87),
           onPressed: () {},
         ),
-        title: Text(
-          _selectedTab == 0
-              ? 'Assistente IA'
-              : _selectedTab == 1
-                  ? 'Criar Imagem'
-                  : _selectedTab == 2
-                      ? 'Tradutor'
-                      : 'Criar Vídeo',
-          style: const TextStyle(
+        title: const Text(
+          'Assistente IA',
+          style: TextStyle(
             color: Colors.black87,
             fontSize: 17,
             fontWeight: FontWeight.w600,
@@ -173,24 +156,25 @@ class _ChatBotFeatureState extends State<ChatBotFeature> {
           if (_isSpeaking)
             const Padding(
               padding: EdgeInsets.only(right: 4),
-              child: Icon(Icons.graphic_eq, color: Color(0xFF6B8EFF), size: 20),
-            ),
-          if (_selectedTab == 0)
-            IconButton(
-              icon: Icon(
-                _ttsEnabled ? Icons.volume_up : Icons.volume_off,
-                color: _ttsEnabled ? const Color(0xFF6B8EFF) : Colors.grey,
-              ),
-              onPressed: () {
-                setState(() => _ttsEnabled = !_ttsEnabled);
-                if (!_ttsEnabled) {
-                  _tts.stop();
-                  _player.stop();
-                }
-              },
+              child: Icon(Icons.graphic_eq,
+                  color: Color(0xFF6B8EFF), size: 20),
             ),
           IconButton(
-            icon: const Icon(Icons.add_circle_outline, color: Colors.black87),
+            icon: Icon(
+              _ttsEnabled ? Icons.volume_up : Icons.volume_off,
+              color: _ttsEnabled ? const Color(0xFF6B8EFF) : Colors.grey,
+            ),
+            onPressed: () {
+              setState(() => _ttsEnabled = !_ttsEnabled);
+              if (!_ttsEnabled) {
+                _tts.stop();
+                _player.stop();
+              }
+            },
+          ),
+          IconButton(
+            icon: const Icon(Icons.add_circle_outline,
+                color: Colors.black87),
             onPressed: () {},
           ),
         ],
@@ -206,198 +190,112 @@ class _ChatBotFeatureState extends State<ChatBotFeature> {
               ),
             ),
           ),
-          if (_selectedTab == 0)
-            Obx(() => ListView(
-                  physics: const BouncingScrollPhysics(),
-                  controller: _c.scrollC,
-                  padding: EdgeInsets.only(
-                      top: mq.height * .02,
-                      bottom: mq.height * .22,
-                      left: 16,
-                      right: 16),
-                  children: _c.list
-                      .map<Widget>((e) => MessageCard(message: e))
-                      .toList(),
-                ))
-          else if (_selectedTab == 1)
-            ImageFeature()
-          else if (_selectedTab == 2)
-            TranslatorFeature()
-          else
-            const Center(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.videocam_rounded, size: 64, color: Colors.grey),
-                  SizedBox(height: 16),
-                  Text('Em breve', style: TextStyle(color: Colors.grey)),
-                ],
-              ),
-            ),
+          Obx(() => ListView(
+                physics: const BouncingScrollPhysics(),
+                controller: _c.scrollC,
+                padding: EdgeInsets.only(
+                    top: mq.height * .02,
+                    bottom: mq.height * .18,
+                    left: 16,
+                    right: 16),
+                children: _c.list
+                    .map<Widget>((e) => MessageCard(message: e))
+                    .toList(),
+              )),
         ],
       ),
-      bottomSheet: _selectedTab == 0
-          ? Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.06),
-                    blurRadius: 20,
-                    offset: const Offset(0, -4),
-                  ),
-                ],
-              ),
-              child: SafeArea(
-                child: Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 12, vertical: 8),
-                      child: SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
-                          children: [
-                            _chip('🎨 Criar Imagem',
-                                () => setState(() => _selectedTab = 1)),
-                            const SizedBox(width: 8),
-                            _chip('🌐 Traduzir',
-                                () => setState(() => _selectedTab = 2)),
-                            const SizedBox(width: 8),
-                            _chip('🎬 Criar Vídeo',
-                                () => setState(() => _selectedTab = 3)),
-                            const SizedBox(width: 8),
-                            _chip('🔍 Pesquisar', () {}),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(12, 0, 12, 8),
-                      child: Row(
-                        children: [
-                          IconButton(
-                            onPressed: () {},
-                            icon: const Icon(Icons.camera_alt_outlined,
-                                color: Colors.grey, size: 22),
-                          ),
-                          Expanded(
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color: const Color(0xFFF5F5F5),
-                                borderRadius: BorderRadius.circular(24),
-                              ),
-                              child: TextFormField(
-                                controller: _c.textC,
-                                onTapOutside: (e) =>
-                                    FocusScope.of(context).unfocus(),
-                                decoration: InputDecoration(
-                                  hintText: _isListening
-                                      ? 'Ouvindo...'
-                                      : 'Digite ou fale algo...',
-                                  hintStyle: TextStyle(
-                                    fontSize: 14,
-                                    color: _isListening
-                                        ? const Color(0xFF6B8EFF)
-                                        : Colors.grey,
-                                  ),
-                                  border: InputBorder.none,
-                                  contentPadding: const EdgeInsets.symmetric(
-                                      horizontal: 16, vertical: 10),
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 8),
-                          GestureDetector(
-                            onTapDown: (_) => _startListening(),
-                            onTapUp: (_) => _stopListening(),
-                            onTapCancel: () => _stopListening(),
-                            child: Container(
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: _isListening
-                                    ? const Color(0xFF6B8EFF)
-                                    : const Color(0xFFF5F5F5),
-                              ),
-                              padding: const EdgeInsets.all(10),
-                              child: Icon(
-                                _isListening ? Icons.mic : Icons.mic_none,
-                                color:
-                                    _isListening ? Colors.white : Colors.grey,
-                                size: 22,
-                              ),
-                            ),
-                          ),
-                          const SizedBox(width: 8),
-                          Container(
-                            decoration: const BoxDecoration(
-                              shape: BoxShape.circle,
-                              gradient: LinearGradient(
-                                colors: [
-                                  Color(0xFF6B8EFF),
-                                  Color(0xFFB06BFF)
-                                ],
-                              ),
-                            ),
-                            child: IconButton(
-                              onPressed: _enviar,
-                              icon: const Icon(Icons.arrow_upward_rounded,
-                                  color: Colors.white, size: 22),
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            )
-          : null,
-      bottomNavigationBar: Container(
-        decoration: const BoxDecoration(
-          border: Border(top: BorderSide(color: Color(0xFFEEEEEE))),
-        ),
-        child: BottomNavigationBar(
-          currentIndex: _selectedTab,
-          onTap: (i) => setState(() => _selectedTab = i),
-          type: BottomNavigationBarType.fixed,
-          backgroundColor: Colors.white,
-          selectedItemColor: const Color(0xFF6B8EFF),
-          unselectedItemColor: Colors.grey,
-          selectedLabelStyle: const TextStyle(
-              fontSize: 11, fontWeight: FontWeight.w600),
-          unselectedLabelStyle: const TextStyle(fontSize: 11),
-          elevation: 0,
-          items: _tabs
-              .map((t) => BottomNavigationBarItem(
-                    icon: Icon(t['icon'] as IconData),
-                    label: t['label'] as String,
-                  ))
-              .toList(),
-        ),
-      ),
-    );
-  }
-
-  Widget _chip(String label, VoidCallback onTap) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+      bottomSheet: Container(
         decoration: BoxDecoration(
-          color: const Color(0xFFF0F0F0),
-          borderRadius: BorderRadius.circular(20),
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.06),
+              blurRadius: 20,
+              offset: const Offset(0, -4),
+            ),
+          ],
         ),
-        child: Text(label,
-            style: const TextStyle(fontSize: 13, color: Colors.black87)),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(12, 8, 12, 8),
+            child: Row(
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.camera_alt_outlined,
+                      color: Colors.grey, size: 22),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFFF5F5F5),
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                    child: TextFormField(
+                      controller: _c.textC,
+                      onTapOutside: (e) =>
+                          FocusScope.of(context).unfocus(),
+                      decoration: InputDecoration(
+                        hintText: _isListening
+                            ? 'Ouvindo...'
+                            : 'Digite ou fale algo...',
+                        hintStyle: TextStyle(
+                          fontSize: 14,
+                          color: _isListening
+                              ? const Color(0xFF6B8EFF)
+                              : Colors.grey,
+                        ),
+                        border: InputBorder.none,
+                        contentPadding: const EdgeInsets.symmetric(
+                            horizontal: 16, vertical: 10),
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                GestureDetector(
+                  onTapDown: (_) => _startListening(),
+                  onTapUp: (_) => _stopListening(),
+                  onTapCancel: () => _stopListening(),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: _isListening
+                          ? const Color(0xFF6B8EFF)
+                          : const Color(0xFFF5F5F5),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Icon(
+                      _isListening ? Icons.mic : Icons.mic_none,
+                      color: _isListening ? Colors.white : Colors.grey,
+                      size: 22,
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                Container(
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    gradient: LinearGradient(
+                      colors: [Color(0xFF6B8EFF), Color(0xFFB06BFF)],
+                    ),
+                  ),
+                  child: IconButton(
+                    onPressed: _enviar,
+                    icon: const Icon(Icons.arrow_upward_rounded,
+                        color: Colors.white, size: 22),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
 }
 
-// Fonte de áudio em bytes para just_audio
+// Fonte de áudio em bytes para just_audio (ElevenLabs TTS)
 class MyBytesAudioSource extends StreamAudioSource {
   final Uint8List _buffer;
   MyBytesAudioSource(this._buffer) : super(tag: 'MyBytesAudioSource');
