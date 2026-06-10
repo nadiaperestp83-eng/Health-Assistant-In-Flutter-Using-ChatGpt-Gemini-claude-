02f70431b041d0f8499d128fc27371103df511d1
nadiaperestp83-eng <nadiaperestp83@gmail.com>
2026-06-10 16:22:47 -0300

    Update tts_service.dart

--- lib/services/tts_service.dart
+++ lib/services/tts_service.dart
@@ -4,6 +4,8 @@ import 'package:path/path.dart' as p;
 import 'package:path_provider/path_provider.dart';
 import 'package:sherpa_onnx/sherpa_onnx.dart';
 import 'package:just_audio/just_audio.dart';
+import 'dart:typed_data';
+
 
 class TtsService {
   static TtsService? _instance;
